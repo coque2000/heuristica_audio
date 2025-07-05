c581803ad2cb14c1a2b953841bc04d79cfc05928
coque2000 <coquesi.jlg@gmail.com>
2025-07-04 18:43:36 -0600

    UPDATE - Change
    Some changes to evaluate the quality

--- scripts/Audio.py
+++ scripts/Audio.py
@@ -10,8 +10,9 @@ import numpy as np
 class Audio:
     def __init__(self, path):
         self.ruta_archivo = path
-        self.audio = None
+        self._audio = None
         self._lib_lectura = None
+        self._rate = None
 
     def leer_audio_pydub(self):
         try:
@@ -20,7 +21,8 @@ class Audio:
             print(f"Error al querer recuperar el audio: {e}")
         try:
             audio = AudioSegment.from_file(self.ruta_archivo)
-            self.audio = audio
+            self._audio = audio
+            self._rate = audio.frame_rate
             self._lib_lectura = "pydub"
             """
             print(f"\n--- Información del archivo: {os.path.basename(self.ruta_archivo)} (con pydub) ---")
@@ -30,16 +32,48 @@ class Audio:
             print(f"Profundidad de bits: {audio.sample_width * 8} bits")
             print(f"Duración: {len(audio) / 1000:.2f} segundos")
             """
-            return audio
+            return audio, audio.frame_rate
         except Exception as e:
             raise Exception(f"Error al leer el archivo con pydub: {e}")
 
-    def leer_audio_
+    def leer_audio_soundfile(self):
+        try:
+            FileUtils.comprobar_directorio(self.ruta_archivo)
+        except Exception as e:
+            print(f"Error al querer recuperar el audio: {e}")
+        try:
+            with sf.SoundFile(self.ruta_archivo, 'r') as f:
+                """
+                print(f"\n--- Información del archivo: {os.path.basename(self.ruta_archivo)} (con soundfile) ---")
+                print(f"Formato: {f.format}")
+                print(f"Subformato: {f.subtype}")
+                print(f"Canales: {f.channels}")
+                print(f"Tasa de muestreo (Hz): {f.samplerate}")
+                print(f"Duración: {f.frames / f.samplerate:.2f} segundos")
+                """
+                data = f.read()
+                # print(f"Shape de los datos de audio (frames, canales): {data.shape}")
+                # return data, f.samplerate
+                self._audio = data
+                self._rate = f.samplerate
+                self._lib_lectura = "soundfile"
+                return data, f.samplerate
+        except Exception as e:
+            raise Exception(f"Error al leer el archivo con soundfile: {e}")
 
+    def convertir_canal_mono(self):
+        if self._audio.ndim > 1:
+            self._audio = np.mean(self._audio, axis=1)
+        return self._audio, self._audio.sample_rate
 
-
+    @property
+    def audio(self):
+        return self._audio
 
     @property
     def lib_lectura(self):
         return self._lib_lectura
 
+    @property
+    def rate(self):
+        return self._rate
--- scripts/EvaluarAudio.py
+++ scripts/EvaluarAudio.py
@@ -1,5 +1,120 @@
-def
+import librosa
+# import webrtcvad
+
+"""
+    Se espera que los valores cumplan lo siguiente:
+    zrc: limpio = 0.02 - 0.08, sucio = >= 0.1
+    spectral_flatness: limpio = 0.1 - 0.3, sucio = >= 0.4
+    rms_energy: limpio = 0.01 - 0.1, sucio = < 000.5 o > 0.15
+"""
+calidad_audio_referencia = {
+    "ZCR": {
+        "rango_bueno": (0.02, 0.08),
+        "rango_malo": (0.1, float("inf")),
+        "interpretacion": "Muy alto indica ruido impulsivo (estática, clics)."
+    },
+    "Spectral_Flatness": {
+        "rango_bueno": (0.1, 0.3),
+        "rango_malo": (0.4, float("inf")),
+        "interpretacion": "Plano = ruido blanco. Bajo = voz con armónicos."
+    },
+    "RMS_Energy": {
+        "rango_bueno": (0.01, 0.1),
+        "rango_malo": [(0, 0.005), (0.15, float("inf"))],
+        "interpretacion": "Bajo = silencio o señal débil. Alto = clipping o distorsión."
+    },
+    "VAD_Voice_Ratio": {
+        "rango_bueno": (0.6, 1.0),
+        "rango_malo": (0, 0.5),
+        "interpretacion": "Proporción de voz detectada. Bajo = silencios o ruido dominante."
+    }
+}
+
+def zero_crosing_rate_mean(audio, sr):
+    """
+    Evalua el número de veces que la señal cambia de signo (de positivo a negativo o viceversa) por segundo.
+    """
+    zrc = librosa.feature.zero_crossing_rate(audio)
+    return zrc.mean()
+
+def spectral_flatness_mean(audio, sr):
+    """
+    Evalua que tan plano es el espectro de frecuencias, una voz natural deberia tener valores bajos ya que es armonica
+    """
+    flatness = librosa.feature.spectral_flatness(y = audio)[0]
+    return flatness.mean()
+
+def rms_energy_mean(audio, sr):
+    """
+    Mide cuan fuerte es la señal en promedio, detecta problemas de volumen o distorsion
+    """
+    rms = librosa.feature.rms(y = audio)[0]
+    return rms.mean()
+
+def ponderar_calidad(zcr: float, spectral_flatness: float, rms_energy: float) -> float:
+    """
+    Pondera los valores de las métricas y proporciona un valor acumulado (0 a 3).
+    """
+    metrica = 0
+
+    zcr_bueno = calidad_audio_referencia["ZCR"]["rango_bueno"]
+    zcr_malo = calidad_audio_referencia["ZCR"]["rango_malo"]
+
+    if zcr_bueno[0] <= zcr <= zcr_bueno[1]:
+        metrica += 1
+    elif zcr >= zcr_malo[0]:
+        metrica += 0.5
+
+    flat_bueno = calidad_audio_referencia["Spectral_Flatness"]["rango_bueno"]
+    flat_malo = calidad_audio_referencia["Spectral_Flatness"]["rango_malo"]
+
+    if flat_bueno[0] <= spectral_flatness <= flat_bueno[1]:
+        metrica += 1
+    elif spectral_flatness >= flat_malo[0]:
+        metrica += 0.5
+
+    rms_bueno = calidad_audio_referencia["RMS_Energy"]["rango_bueno"]
+    rms_malo = calidad_audio_referencia["RMS_Energy"]["rango_malo"]
+
+    if rms_bueno[0] <= rms_energy <= rms_bueno[1]:
+        metrica += 1
+    elif any(lower <= rms_energy <= upper for (lower, upper) in rms_malo):
+        metrica += 0.5
+
+    return round(metrica, 2)
+
+"""
+def vad_voice_ratio(audio, sr, frame_duration_ms=30):
+    vad = webrtcvad.Vad()
+    vad.set_mode(1)  # 0: menos agresivo, 3: muy agresivo
+
+    # Convertir a formato 16-bit PCM
+    import struct
+    audio_int16 = (audio * 32768).astype('int16')
+    audio_bytes = struct.pack('<' + 'h'*len(audio_int16), *audio_int16)
+
+    frame_length = int(sr * frame_duration_ms / 1000)
+    num_frames = len(audio_bytes) // (2 * frame_length)
+
+    voiced = 0
+    total = 0
+
+    for i in range(num_frames):
+        start = i * frame_length * 2
+        end = start + frame_length * 2
+        frame = audio_bytes[start:end]
+
+        if len(frame) < frame_length * 2:
+            break
+        is_speech = vad.is_speech(frame, sr)
+        voiced += int(is_speech)
+        total += 1
+
+    return voiced / total if total > 0 else 0.0
+"""
+
 
 
 if __name__ == "__main__":
-    print(__doc__)
+    ponderacion = ponderar_calidad(zcr = 0.1, spectral_flatness = 0.1, rms_energy = 0.01)
+    print(ponderacion)
